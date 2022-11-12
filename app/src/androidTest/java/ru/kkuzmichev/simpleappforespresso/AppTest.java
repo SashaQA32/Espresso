43a600c3900ef4effe7b740f0d76cc9861d0dcaa
Александр Стрюков <sanyajump16@gmail.com>
2022-11-12 21:19:33 +0300

    Added Intent test

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AppTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AppTest.java
@@ -1,11 +1,22 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.CoreMatchers.allOf;
+
+import android.content.Intent;
+
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -29,5 +40,20 @@ public class AppTest {
                         withText("This is home fragment")
                 )
         );
+    }
+
+    @Test
+    public void intentTest() {
+        ViewInteraction settingsButton = onView(withContentDescription("More options"));
+        ViewInteraction settingsTextView = onView(withId(R.id.title));
+
+        settingsButton.check(matches(isDisplayed()));
+        settingsButton.perform(click());
+
+        Intents.init();
+        settingsTextView.perform(click());
+        intended(allOf(hasData("https://google.com"),
+                hasAction(Intent.ACTION_VIEW)));
+        Intents.release();
     }
 }
